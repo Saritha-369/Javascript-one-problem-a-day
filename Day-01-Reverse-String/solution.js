7112393cb214cad4e7f66c66a15515248b237a30
Saritha R <90122146+Saritha-369@users.noreply.github.com>
2026-06-03 07:46:03 +0530

    Update solution.js

--- Day-01-Reverse-String/solution.js
+++ Day-01-Reverse-String/solution.js
@@ -1,1 +1,22 @@
+// Method 1: Using Built-in Methods
 
+function reverseStringBuiltIn(str) {
+    return str.split('').reverse().join('');
+}
+
+console.log(reverseStringBuiltIn("hello"));
+
+
+// Method 2: Without Built-in Methods
+
+function reverseString(str) {
+    let reversed = '';
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i];
+    }
+
+    return reversed;
+}
+
+console.log(reverseString("hello"));
