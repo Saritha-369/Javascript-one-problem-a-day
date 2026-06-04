9fc559f08a4cf5f8d138ef49c6256218d4868cd6
Saritha R <90122146+Saritha-369@users.noreply.github.com>
2026-06-04 22:57:50 +0530

    Update solution.js

--- Day-02-Count-Vowels/solution.js
+++ Day-02-Count-Vowels/solution.js
@@ -1,1 +1,41 @@
+//Method 1: Using includes()
 
+function countVowels(str) {
+    let count = 0;
+    const vowels = "aeiou";
+
+    for (let char of str.toLowerCase()) {
+        if (vowels.includes(char)) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+console.log(countVowels("hello"));
+console.log(countVowels("JavaScript"));
+
+//Method 2: Without includes()
+function countVowels(str) {
+    let count = 0;
+
+    str = str.toLowerCase();
+
+    for (let i = 0; i < str.length; i++) {
+        if (
+            str[i] === 'a' ||
+            str[i] === 'e' ||
+            str[i] === 'i' ||
+            str[i] === 'o' ||
+            str[i] === 'u'
+        ) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+console.log(countVowels("hello"));
+console.log(countVowels("JavaScript"));
